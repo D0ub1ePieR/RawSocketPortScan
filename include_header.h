c1ed293cc7731fd5cf38283caf48bad0568156dc
D0ub1ePieR <37244096+D0ub1ePieR@users.noreply.github.com>
2018-04-22 23:24:14 +0800

    添加需要的定义参数

--- include_header.h
+++ include_header.h
@@ -15,15 +15,36 @@
 #include<netinet/in.h>	//获取protocol参数的常值
 #include<netinet/ip.h>	//ip帧头定义
 #include<netinet/ip_icmp.h>	//icmp帧头定义
+#include<pthread.h>
 
 #define maxbuf 2048
 
+//global
 struct scansock
 {
-	char src_ip[maxbuf];
-	char dst_ip[maxbuf];
-	int start_port;
-	int end_port;
+	char src_ip[maxbuf];	//源ip地址
+	char dst_ip[maxbuf];	//目标ip地址
+	int start_port;			//扫描起始端口
+	int end_port;			//扫描结束端口
 };
 
 int pingflag;
+
+//tcp_connect_scan_port
+struct list
+{
+	int data;
+	struct list *next;
+};
+
+struct list *exist_port=NULL;
+
+int con_cnt;  
+static pthread_mutex_t connect_printf_mutex = PTHREAD_MUTEX_INITIALIZER;  
+static pthread_mutex_t connect_num_mutex = PTHREAD_MUTEX_INITIALIZER; 
+
+//tcp_syn_scan_port
+int syn_cnt;  
+static pthread_mutex_t syn_printf_mutex = PTHREAD_MUTEX_INITIALIZER;  
+static pthread_mutex_t syn_num_mutex = PTHREAD_MUTEX_INITIALIZER; 
+
